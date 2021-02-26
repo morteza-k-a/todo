02fd47983c89c9eff75084d4a772446e84abba25
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2021-02-26 17:26:00 +0330

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import Todo from "./components/todoitem";
 
-function App() {
+export default function App() {
+  const [list, setlist] = useState([{ key: uuidv4(), text: "item 1" }]);
+  const [text, settext] = useState("");
+  const genuuid = () => {
+    while (true) {
+     var uuid_ = uuidv4();
+     var formattedls = list.filter((item) => item.key === uuid_);
+      if (formattedls.length === 0) return uuid_;
+    }
+  };
+  const delitem = (key) => {
+    setlist((ls) => ls.filter((item) => item.key !== key));
+  };
+  const additem = () => {
+    setlist((ls) => ls.concat({ key: genuuid(), text: text }));
+    settext("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addtodo">
+
+          <TextField
+            style={{ width: "75%",marginRight:"5%" }}
+            value={text}
+            onChange={(e) => settext(e.target.value)}
+            placeholder={"task to do"}
+          />
+
+        <Button
+          variant={"contained"}
+          style={{ width: "20%" }}
+          color={"primary"}
+          onClick={additem}
         >
-          Learn React
-        </a>
-      </header>
+
+          add
+        </Button>
+      </div>
+      {list.map((item) => {
+        return (
+          <Todo deletefunction={delitem} item={item}/>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
---
+++ src/components/todoitem.js
@@ -0,0 +1,38 @@
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import DoneOutlinedIcon from "@material-ui/icons/DoneOutlined";
+import {Component} from "react";
+import {Paper,Typography,TextField} from "@material-ui/core"
+
+class Todo extends Component{
+    constructor(props){
+        super(props);
+        this.state = {text:props.item.text,editing:false};
+    }
+    changetext = (e)=>{
+        this.setState(st => ({text:e.target.value ,editing:st.editing}))
+    }
+    changeedit=()=>{
+
+        this.setState(st => ({text:this.state.text ,editing:!st.editing}))
+    }
+    render(){
+        return(
+            <Paper className="todo">
+            <div className="info">
+              {!this.state.editing?<Typography>{this.state.text}</Typography>:<TextField value={this.state.text} onChange={this.changetext} style={{width:"95%"}}/>}
+            </div>
+            <div className="control">
+             {this.state.editing?<DoneOutlinedIcon onClick={this.changeedit} style={{cursor:"pointer"}}/>:<EditOutlinedIcon onClick={this.changeedit} style={{cursor:"pointer"}}/>}
+              <DeleteOutlineIcon
+style={{cursor:"pointer"}}
+                onClick={() => {
+                  this.props.deletefunction(this.props.item.key);
+                }}
+              />
+            </div>
+          </Paper>
+        )
+    }
+}
+export default Todo;
