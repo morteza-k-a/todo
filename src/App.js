128ee0fd3d8adbd00e7e45f75812cb9710057ec6
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2021-03-05 19:15:18 +0330

    add pwa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { v4 as uuidv4 } from "uuid";
+import { useState,useEffect } from "react";
+import { TextField, Button,Paper,Typography } from "@material-ui/core";
+import Alert from '@material-ui/lab/Alert';
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import DoneOutlinedIcon from "@material-ui/icons/DoneOutlined";
 
-function App() {
+export default function App() {
+  const [list, setlist] = useState([]);
+  const [text, settext] = useState("");
+  const [edittext, setedittext] = useState("");
+  const [editindex,setindex] = useState(null);
+  const genuuid = () => {
+    while (true) {
+     var uuid_ = uuidv4();
+     var formattedls = list.filter((item) => item.key === uuid_);
+      if (formattedls.length === 0) return uuid_;
+    }
+  };
+  const delitem = (key) => {
+    setlist((ls) => ls.filter((item) => item.key !== key));
+  };
+  const additem = () => {
+    setlist((ls) => ls.concat({ key: genuuid(), text: text }));
+    settext("");
+  };
+  const edititem=(key,text)=>{
+   const listdup = list;
+     for (let i in listdup){
+        if(listdup[i].key === key )
+           listdup[i].text = text
+     }
+    setlist(listdup)
+}
+    useEffect(()=>{
+      if(localStorage.todo)
+        setlist(JSON.parse(localStorage.todo))
+    },[])
+    useEffect(()=>{
+      if(localStorage)
+        localStorage.setItem("todo",JSON.stringify(list));
+    },[list])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addtodo">
+
+          <TextField
+            style={{ width: "75%",marginRight:"5%" }}
+            value={text}
+            onChange={(e) => settext(e.target.value)}
+            placeholder={"task to do"}
+          />
+
+        <Button
+          variant={"contained"}
+          style={{ width: "20%" }}
+          color={"primary"}
+          onClick={additem}
         >
-          Learn React
-        </a>
-      </header>
+
+          add
+        </Button>
+      </div>
+      {
+        list.length !== 0?
+      list.map((item,index)=>{return(
+            <Paper className="todo">
+            <div className="info">
+              {!(editindex === index)?<Typography>{item.text}</Typography>:<TextField value={edittext?edittext:item.text} onChange={e=>setedittext(e.target.value)} style={{width:"95%"}}/>}
+            </div>
+            <div className="control">
+             {editindex === index?<DoneOutlinedIcon onClick={()=>{setindex(null);setedittext("");edititem(item.key,edittext)}} style={{cursor:"pointer"}}/>:<EditOutlinedIcon onClick={()=>{setindex(index)}} style={{cursor:"pointer"}}/>}
+              <DeleteOutlineIcon
+style={{cursor:"pointer"}}
+                onClick={() => {
+                  delitem(item.key);
+                }}
+              />
+            </div>
+          </Paper>)
+    }):<Alert style={{margin:".5rem"}} severity="info">your tasks will be shown here!</Alert>
+
+    }
     </div>
   );
 }
-
-export default App;
